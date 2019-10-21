b4ad97f40b244a1c18654c39956133f2727d8efe
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2019-10-21 15:43:30 +0900

    make decoder more debuggable

--- instr.c
+++ instr.c
@@ -3,55 +3,56 @@
 #include "instr.h"
 #include "state.h"
 #include "logging.h"
+#include "exec.h"
   
 void set_r_instr(int from, instr_r_t *to){
-  to->funct7 = from >> 25;
-  to->rs2 = (from >> 20) & 0b11111;
-  to->rs1 = (from >> 15) & 0b11111;
-  to->funct3 = (from >> 12) & 0b111;
-  to->rd = (from >> 7) & 0b11111;
+  to->funct7 = srl(from, 25);
+  to->rs2 = srl(from, 20) & 0b11111;
+  to->rs1 = srl(from, 15) & 0b11111;
+  to->funct3 = srl(from, 12) & 0b111;
+  to->rd = srl(from, 7) & 0b11111;
 }
 
 void set_i_instr(int from, instr_i_t *to){
-  to->imm = from >> 20;
-  to->rs1 = (from >> 15) & 0b11111;
-  to->funct3 = (from >> 12) & 0b111;
-  to->rd = (from >> 7) & 0b11111;
+  to->imm = srl(from, 20);
+  to->rs1 = srl(from, 15) & 0b11111;
+  to->funct3 = srl(from, 12) & 0b111;
+  to->rd = srl(from, 7) & 0b11111;
   
   to->imm = SIGNEXT(to->imm, 11);
 }
 
 void set_s_instr(int from, instr_s_t *to){
-  to->imm = ((from & 0xFE000000) >> 20) | ((from & 0xF80) >> 7);
-  to->rs2 = (from >> 20) & 0b11111;
-  to->rs1 = (from >> 15) & 0b11111;
-  to->funct3 = (from >> 12) & 0b111;
+  to->imm = srl((from & 0xFE000000), 20) | srl((from & 0xF80), 7);
+  to->rs2 = srl(from, 20) & 0b11111;
+  to->rs1 = srl(from, 15) & 0b11111;
+  to->funct3 = srl(from, 12) & 0b111;
 
   to->imm = SIGNEXT(to->imm, 11);
 }
 
 void set_b_instr(int from, instr_b_t *to){
-  to->imm = ((from & 0x80000000) >> 19) | ((from & 0x80) << 4) | ((from & 0x7E000000) >> 20) | ((from & 0xF00) >> 7);
-  to->rs2 = (from >> 20) & 0b11111;
-  to->rs1 = (from >> 15) & 0b11111;
-  to->funct3 = (from >> 12) & 0b111;
+  to->imm = srl((from & 0x80000000), 19) | ((from & 0x80) << 4) | srl((from & 0x7E000000), 20) | srl((from & 0xF00), 7);
+  to->rs2 = srl(from, 20) & 0b11111;
+  to->rs1 = srl(from, 15) & 0b11111;
+  to->funct3 = srl(from, 12) & 0b111;
 
   to->imm = SIGNEXT(to->imm, 12);
 }
 
 void set_u_instr(int from, instr_u_t *to){
   to->imm = from & (~0b111111111111);
-  to->rd = (from >> 7) & 0b11111;
+  to->rd = srl(from, 7) & 0b11111;
 }
 
 void set_j_instr(int from, instr_j_t *to){
-  to->imm = ((from & 0x80000000) >> 11) | (from & 0xFF000) | ((from & 0x100000) >> 9) | ((from & 0x7FE00000) >> 20);
-  to->rd = (from >> 7) & 0b11111;
+  to->imm = srl((from & 0x80000000), 11) | (from & 0xFF000) | srl((from & 0x100000), 9) | srl((from & 0x7FE00000), 20);
+  to->rd = srl(from, 7) & 0b11111;
   to->imm = SIGNEXT(to->imm, 20);
 }
 
-void unimplemented(){
-  error(":thinking_face:");
+void unimplemented(int iraw, int loc){
+  error("unimplemented instr: %08x (internal state: %d)", iraw, loc);
   exit(1);
 }
 
@@ -86,7 +87,7 @@ instr_t *fetch_and_decode_once(state_t *state){
     break;
   case 0b1100011: // (B*)
     set_b_instr(iraw, (instr_b_t*) instr);
-    switch((iraw & 0x7000) >> 12){
+    switch(srl(iraw & 0x7000, 12)){
     case 0b000: // BEQ
       instr->op = BEQ;
       break;
@@ -109,7 +110,7 @@ instr_t *fetch_and_decode_once(state_t *state){
     break;
   case 0b0000011: // (L*)
     set_i_instr(iraw, (instr_i_t*) instr);
-    switch((iraw & 0x7000) >> 12){
+    switch(srl(iraw & 0x7000, 12)){
     case 0b000: // LB
       instr->op = LB;
       break;
@@ -129,7 +130,7 @@ instr_t *fetch_and_decode_once(state_t *state){
     break;
   case 0b0100011: // (S*)
     set_s_instr(iraw, (instr_s_t*) instr);
-    switch((iraw & 0x7000) >> 12){
+    switch(srl(iraw & 0x7000, 12)){
     case 0b000: // SB
       instr->op = SB;
       break;
@@ -140,13 +141,13 @@ instr_t *fetch_and_decode_once(state_t *state){
       instr->op = SW;
       break;
     default:
-      unimplemented();
+      unimplemented(iraw, 0);
       break;
     }
     break;
   case 0b0010011: // (Arith Imm)
     set_i_instr(iraw, (instr_i_t*) instr);
-    switch((iraw & 0x7000) >> 12){
+    switch(srl(iraw & 0x7000, 12)){
     case 0b000: // ADDI
       instr->op = ADDI;
       break;
@@ -169,7 +170,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       instr->op = SLLI;
       break;      
     case 0b101: // (SRLI or SRAI)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // SRLI
         instr->op = SRLI;
         break;
@@ -182,9 +183,9 @@ instr_t *fetch_and_decode_once(state_t *state){
     break;
   case 0b0110011: // (Arith Other)
     set_r_instr(iraw, (instr_r_t*) instr);
-    switch((iraw & 0x7000) >> 12){
+    switch(srl(iraw & 0x7000, 12)){
     case 0b000: // (ADD or SUB or MUL)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // ADD
         instr->op = ADD;
         break;
@@ -197,7 +198,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;
     case 0b001: // (SLL or MULH)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // SLL
         instr->op = SLL;
         break;
@@ -207,7 +208,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;
     case 0b010: // (SLT or MULHSU)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // SLT
         instr->op = SLT;
         break;
@@ -217,7 +218,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;      
     case 0b011: // (SLTU or MULHU)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // SLTU
         instr->op = SLTU;
         break;
@@ -227,7 +228,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;      
     case 0b100: // (XOR or DIV)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // XOR
         instr->op = XOR;
         break;
@@ -237,7 +238,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;      
     case 0b101: // (SRL or SRA or DIVU)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // SRL
         instr->op = SRL;
         break;
@@ -250,7 +251,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;      
     case 0b110: // (OR or REM)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // OR
         instr->op = OR;
         break;
@@ -260,7 +261,7 @@ instr_t *fetch_and_decode_once(state_t *state){
       }
       break;      
     case 0b111: // (AND or REMU)
-      switch(iraw >> 25){
+      switch(srl(iraw, 25)){
       case 0b0000000: // AND
         instr->op = AND;
         break;
@@ -277,74 +278,74 @@ instr_t *fetch_and_decode_once(state_t *state){
     break;
   case 0b1110011:
     // TODO: rv32i Others
-    unimplemented();
+    unimplemented(iraw, 1);
     break;
 
     // rv32f
     //////////
   case 0b0000111: // FLW
-    if((iraw & 0x7000) >> 12 == 0b010){
+    if(srl(iraw & 0x7000, 12) == 0b010){
       set_i_instr(iraw, (instr_i_t*) instr);
       instr->op = FLW;
       break;
     } else {
-      unimplemented();
+      unimplemented(iraw, 2);
       break;
     }
   case 0b0100111: // FSW
-    if((iraw & 0x7000) >> 12 == 0b010){
+    if(srl(iraw & 0x7000, 12) == 0b010){
       set_s_instr(iraw, (instr_s_t*) instr);
       instr->op = FSW;
       break;
     } else {
-      unimplemented();
+      unimplemented(iraw, 3);
       break;
     }
   case 0b1010011: // Floating Arith
     set_r_instr(iraw, (instr_r_t*) instr);
-    switch(iraw >> 25){
+    switch(srl(iraw, 25)){
     case 0b0000000: // FADDS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FADDS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 4);
         break;
       }
     case 0b0000100: // FSUBS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FSUBS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 5);
         break;
       }
     case 0b0001000: // FMULS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FMULS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 6);
         break;
       }
     case 0b0001100: // FDIVS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FDIVS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 7);
         break;
       }
     case 0b0101100: // FSQRTS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FSQRTS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 8);
         break;
       }
     case 0b0010000: // FSGNJS, FSGNJNS, FSGNJXS
-      switch ((iraw & 0x7000) >> 12){
+      switch (srl(iraw & 0x7000, 12)){
       case 0b000:
         instr->op = FSGNJS;
         break;
@@ -355,28 +356,28 @@ instr_t *fetch_and_decode_once(state_t *state){
         instr->op = FSGNJXS;
         break;
       default:
-        unimplemented();
+        unimplemented(iraw, 8);
         break;
       }
       break;
     case 0b1100000: // FCVTWS
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FCVTWS;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 9);
         break;
       }
     case 0b1110000: // FMVXW
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FMVXW;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 10);
         break;
       }
     case 0b1010000: // FEQS, FLES
-      switch ((iraw & 0x7000) >> 12){
+      switch (srl(iraw & 0x7000, 12)){
       case 0b010:
         instr->op = FEQS;
         break;
@@ -384,33 +385,33 @@ instr_t *fetch_and_decode_once(state_t *state){
         instr->op = FLES;
         break;
       default:
-        unimplemented();
+        unimplemented(iraw, 11);
         break;
       }
       break;
     case 0b1101000: // FCVTSW
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FCVTSW;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 12);
         break;
       }
     case 0b1111000: // FMVWX
-      if((iraw & 0x7000) >> 12 == 0b000){
+      if(srl(iraw & 0x7000, 12) == 0b000){
         instr->op = FMVWX;
         break;
       } else {
-        unimplemented();
+        unimplemented(iraw, 13);
         break;
       }
     default:
-      unimplemented();
+      unimplemented(iraw, 14);
       break;
     }
     break;
   default:
-    unimplemented();
+    unimplemented(iraw, 15);
     break;
   }
   return instr;
